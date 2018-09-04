7dc08f58bd19a84cf9895546656707f3dae9c891
Martin Charles <martincharles07@gmail.com>
2018-09-04 02:26:24 -0400

    Added Google Stuff
    It's stupid that I had to write this. Google's eng is just hard cs
    problems?

---
+++ configurator/src/GAPI.ts
@@ -0,0 +1,58 @@
+export interface GAPI {
+  load: (api: string, callback: () => void) => void;
+  signin2: SignIn2;
+  auth2: Auth2;
+}
+
+export interface Auth2 {
+  init: (opts: { clientId: string }) => Auth2Instance;
+}
+
+export interface Auth2Instance {
+  isSignedIn: LiveData;
+  currentUser: LiveData;
+}
+
+interface LiveData {
+  listen: (callback: (args: any) => void) => void;
+}
+
+export interface SignIn2 {
+  render: (
+    elemenet: HTMLElement,
+    opts: {
+      scope: string;
+      onsuccess: (user: GoogleUser) => void;
+      onfailure: (err: any) => void;
+    }
+  ) => void;
+}
+
+export interface GoogleUser {
+  getAuthResponse(): AuthResponse;
+}
+
+export interface AuthResponse {
+  id_token: string;
+}
+
+const loadScript = (url: string): Promise<void> => {
+  const script = document.createElement("script");
+  script.src = url;
+  document.head.appendChild(script);
+
+  return new Promise((resolve, reject) => {
+    script.onload = () => resolve();
+    script.onerror = reject;
+  });
+};
+
+export const getGapi = async (): Promise<GAPI> => {
+  await loadScript("https://apis.google.com/js/platform.js");
+  return (window as any).gapi;
+};
+
+export const loaded = (gapi: GAPI, api: string): Promise<void> =>
+  new Promise(resolve => {
+    gapi.load(api, () => resolve());
+  });
---
+++ configurator/src/GoogleSignIn.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+import {
+  GAPI,
+  loaded,
+  getGapi,
+  GoogleUser,
+  Auth2Instance,
+  AuthResponse
+} from "./GAPI";
+
+interface Props {
+  clientId: string;
+
+  onSignInChanged: (response: AuthResponse) => void;
+}
+
+export class GoogleSignIn extends React.Component<Props> {
+  private async initSignIn(gapi: GAPI): Promise<Auth2Instance> {
+    await loaded(gapi, "auth2");
+    const auth2 = gapi.auth2.init({ clientId: this.props.clientId });
+    return auth2;
+  }
+
+  public async componentDidMount() {
+    const gapi = await getGapi();
+    const auth2 = await this.initSignIn(gapi);
+
+    auth2.isSignedIn.listen(console.log);
+    auth2.currentUser.listen(console.log);
+  }
+
+  private onRef = async (e: HTMLDivElement | null) => {
+    if (e == null) {
+      return;
+    }
+
+    const gapi = await getGapi();
+    await this.initSignIn(gapi);
+
+    gapi.signin2.render(e, {
+      scope: "profile email",
+      onsuccess: this.onSuccess,
+      onfailure: console.log
+    });
+  };
+
+  private onSuccess = (googleUser: GoogleUser) => {
+    const authResponse = googleUser.getAuthResponse();
+    this.props.onSignInChanged(authResponse);
+  };
+
+  public render() {
+    return <div style={{ height: 36, width: 120 }} ref={this.onRef} />;
+  }
+}
